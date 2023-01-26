9b4a1d4fb57b17b42b2d825c7fa10cbb3c49b2da
Jaehun <55674750+Hyika@users.noreply.github.com>
2023-01-26 15:07:17 +0900

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:todo_project/main_todo.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
+    // Statusbar Settings
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent,
+    ));
 
     return MaterialApp(
       title: 'Todo Project',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            TodoWidget(),
-          ],
+        backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+        resizeToAvoidBottomInset: true,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TodoWidget(),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/main_todo.dart
+++ lib/main_todo.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+int cnt = 0;
+String? inputText;
+List<String> txtList = [];
+
 class TodoWidget extends StatefulWidget {
   const TodoWidget({super.key});
 
@@ -8,29 +12,101 @@ class TodoWidget extends StatefulWidget {
 }
 
 class _TodoWidgetState extends State<TodoWidget> {
+  final _txtController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final deviceWidth = MediaQuery.of(context).size.width;
-    final deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double deviceHeight = MediaQuery.of(context).size.height;
 
-    return Column(
-      children: <Widget>[
-        Container(
-          alignment: Alignment.centerLeft,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-          child: Text(
-            '오늘 할 일',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // Title
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              child: Text(
+                '오늘 할 일',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
             ),
-          ),
+            // Today's Todo list
+            Container(
+              height: deviceHeight * 0.45,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ListItemBuilder(),
+            ),
+            // add button
+            Container(
+              child: Container(
+                height: 40,
+                margin: const EdgeInsets.all(11),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))
+                ),
+                child: Row(
+                  children: <Widget>[
+                    // Row - Icon area
+                    GestureDetector(
+                      onTap: () => setState(() {
+                        if (inputText != null && inputText != '') {
+                          txtList.add(inputText!);
+                          _txtController.clear();
+                          cnt++;
+                          inputText = '';
+                        }
+                      }),
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: const Icon(
+                          Icons.add,
+                        ),
+                      ),
+                    ),
+                    // Row - Text area
+                    SizedBox(
+                      width: deviceWidth * 0.7,
+                      child: TextField(
+                        controller: _txtController,
+                        textAlignVertical: TextAlignVertical(y: 0.5),
+                        onChanged: (txt) => setState(() {
+                          inputText = txt;
+                        }),
+                        onSubmitted: (_) {
+                          if (inputText != null && inputText != '') {
+                            txtList.add(inputText!);
+                            _txtController.clear();
+                            cnt++;
+                            inputText = '';
+                          }
+                        },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '오늘은 어떤 일을 할까요?',
+                          hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-        Container(
-          height: deviceHeight * 0.45,
-          child: ListItemBuilder(),
-        ),
-      ],
+      ),
     );
   }
 }
@@ -44,65 +120,46 @@ class ListItemBuilder extends StatefulWidget {
 }
 
 class _ListItemBuilderState extends State<ListItemBuilder> {
-  int cnt = 1;
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      padding: const EdgeInsets.all(8),
       itemCount: cnt,
       itemBuilder: (context, index) {
-        if (index == cnt - 1) {
-          return GestureDetector(
-            onTap: () => setState(() {
-              cnt++;
-            }),
-            child: Container(
-              height: 30,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 161, 161, 161),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: const Icon(
-                      Icons.add,
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.only(left: 5.0),
-                    child: const Text('일정을 추가하려면 여기를 눌러주세요.'),
-                  ),
-                ],
-              ),
-            ),
-          );
-        } else
-          return Container(
-            padding: EdgeInsets.only(left: 4.0),
-            height: 30.0,
-            child: Row(
-              children: <Widget>[
-                Container(
+        // list item container
+        return Container(
+          margin: const EdgeInsets.all(3),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: Row(
+            children: <Widget>[
+              // Row - Icon area
+              GestureDetector(
+                onTap: () => setState(() {
+
+                }),
+                child: Container(
+                  padding: EdgeInsets.all(10),
                   child: Icon(
                     Icons.circle_outlined,
-                    size: 15
+                    size: 20,
+                    color: Colors.lightGreen,
                   ),
                 ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    'Test',
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
+              ),
+              // Row - Text area
+              SizedBox(
+                child: Text(
+                  txtList[index],
+                  style: TextStyle(
+                    fontSize: 15,
                   ),
-                )
-              ],
-            ),
-          );
+                ),
+              )
+            ],
+          ),
+        );
       },
     );
   }
