25cea56817bbbe2c08d6b9297341717e2d3c9155
Jaehun <55674750+Hyika@users.noreply.github.com>
2023-01-31 16:46:20 +0900

    v1.0.1
    require modify jank

--- lib/todo.dart
+++ lib/todo.dart
@@ -79,40 +79,37 @@ class _TodoWidgetState extends State<TodoWidget> {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double deviceHeight = MediaQuery.of(context).size.height;
 
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: SingleChildScrollView(
-        child: Container(
-          height: deviceHeight * 0.45,
-          margin: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: ListView.builder(
-            itemCount: _txtList.length,
-            itemBuilder: (context, index) {
-              final String item = _txtList[index];
-              return Container(
-                margin: const EdgeInsets.all(3),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(13),
-                  child: Dismissible(
-                    key: Key(item),
-                    background: leftSlideBackground(),
-                    // Delete item
-                    secondaryBackground: rightSlideBackground(),
-                    // Complete item
-                    onDismissed: (direction) {
-                      setState(() {
-                        _txtList.removeAt(index);
-                      });
-                    },
-                    child: todoListBuilder(item),
-                  ),
+    return SingleChildScrollView(
+      child: Container(
+        height: deviceHeight * 0.45,
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListView.builder(
+          itemCount: _txtList.length,
+          itemBuilder: (context, index) {
+            final String item = _txtList[index];
+            return Container(
+              margin: const EdgeInsets.all(3),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(13),
+                child: Dismissible(
+                  key: Key(item),
+                  background: leftSlideBackground(),
+                  // Delete item
+                  secondaryBackground: rightSlideBackground(),
+                  // Complete item
+                  onDismissed: (direction) {
+                    setState(() {
+                      _txtList.removeAt(index);
+                    });
+                  },
+                  child: todoListBuilder(item),
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -128,49 +125,177 @@ class TextFieldBar extends StatefulWidget {
 
 class _TextFieldBarState extends State<TextFieldBar> {
   final _txtController = TextEditingController();
+  bool _tapped = false;
 
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double deviceHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      height: 40,
-      margin: const EdgeInsets.all(11),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10.0))),
-      child: Row(
-        children: <Widget>[
-          // Row - Icon area
-          Container(
-            padding: EdgeInsets.all(8),
-            child: const Icon(
-              Icons.add,
-            ),
-          ),
-          SizedBox(
-            width: deviceWidth * 0.7,
-            child: TextField(
-              controller: _txtController,
-              textAlignVertical: TextAlignVertical.center,
-              onSubmitted: (value) {
-                if (value != '') {
-                  _txtList.add(value);
-                  _txtController.clear();
-                }
-              },
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: '오늘은 어떤 일을 할까요?',
-                hintStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
+    return GestureDetector(
+      onTap: () => setState(() {
+        _tapped = !_tapped;
+      }),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
+        height: _tapped ? 100 : 40,
+        width: deviceWidth,
+        margin: const EdgeInsets.all(11),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Visibility(
+                visible: !_tapped,
+                child: Container(
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    '일정을 추가하려면 눌러주세요',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 136, 136, 136),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Visibility(
+                visible: _tapped,
+                child: Container(
+                  height: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          height: 46,
+                          margin: EdgeInsets.fromLTRB(6, 6, 6, 3),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              GestureDetector(
+                                  child: Container(
+                                width: deviceWidth * 0.27,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 233, 235, 238),
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: const <Widget>[
+                                    Icon(
+                                      Icons.alarm,
+                                      color: Colors.black,
+                                      size: 18,
+                                    ),
+                                    SizedBox(
+                                      child: Text(
+                                        '시간 설정',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                              GestureDetector(
+                                  child: Container(
+                                width: deviceWidth * 0.27,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 233, 235, 238),
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: const <Widget>[
+                                    Icon(
+                                      Icons.calendar_month,
+                                      color: Colors.black,
+                                      size: 18,
+                                    ),
+                                    SizedBox(
+                                      child: Text(
+                                        '기한 설정',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                              GestureDetector(
+                                  child: Container(
+                                width: deviceWidth * 0.27,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 233, 235, 238),
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: const <Widget>[
+                                    Icon(
+                                      Icons.notification_add,
+                                      color: Colors.black,
+                                      size: 18,
+                                    ),
+                                    SizedBox(
+                                      child: Text(
+                                        '알림 설정',
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                            ],
+                          )),
+                      Container(
+                        height: 36,
+                        margin: EdgeInsets.fromLTRB(12, 0, 12, 9),
+                        child: TextField(
+                          controller: _txtController,
+                          autofocus: true,
+                          textAlignVertical: TextAlignVertical.center,
+                          onSubmitted: (value) {
+                            if (value != '') {
+                              _txtList.add(value);
+                              _txtController.clear();
+                              _tapped = !_tapped;
+                            }
+                          },
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '오늘은 어떤 일을 할까요?',
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
