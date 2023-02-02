73afd3ea1159cbba6aa9197983222712837f8330
Jaehun <55674750+Hyika@users.noreply.github.com>
2023-02-02 21:13:39 +0900

    v1.0.2
    +add green bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-List<String> _txtList = [];
+import 'package:todo_project/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Statusbar Settings
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-      statusBarIconBrightness: Brightness.dark,
       statusBarColor: Colors.transparent,
     ));
 
@@ -38,234 +36,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 1,
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyTodoPage()),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: const Text(
-                      '오늘 할 일',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Center(
-              child: GestureDetector(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyTodoPage()),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: const Text(
-                    '오늘 할 일',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyTodoPage extends StatefulWidget {
-  const MyTodoPage({super.key});
-
-  @override
-  State<MyTodoPage> createState() => _MyTodoPageState();
-}
-
-class _MyTodoPageState extends State<MyTodoPage> {
-  final _txtController = TextEditingController();
-  bool _tapped = false;
-
-  void _handleSubmitted(String text) {
-    if (text != '') {
-      setState(() {
-        _txtList.add(text);
-        _txtController.clear();
-        _tapped = !_tapped;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double deviceWidth = MediaQuery.of(context).size.width;
-    final double deviceHeight = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 236, 236, 236),
-      resizeToAvoidBottomInset: true,
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
-            child: const Text(
-              '오늘 할 일',
-              style: TextStyle(
+  Widget titleInkWellBar(_onTap, String title) {
+    return InkWell(
+      onTap: _onTap,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(18, 10, 18, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 22,
                 fontWeight: FontWeight.bold,
-                fontSize: 20,
               ),
             ),
-          ),
-          Expanded(
-            child: TodoWidget(),
-          ),
-        ],
-      ),
-      bottomSheet: GestureDetector(
-        onTap: () => setState(() {
-          _tapped = !_tapped;
-        }),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 100),
-          height: _tapped ? 100 : 40,
-          width: deviceWidth,
-          margin: const EdgeInsets.all(12),
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(13.0))),
-          child: SingleChildScrollView(
-            physics: const NeverScrollableScrollPhysics(),
-            child: Column(
-              children: <Widget>[
-                Visibility(
-                  visible: !_tapped,
-                  child: Container(
-                    height: 40,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      '작업을 추가하려면 눌러주세요',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 136, 136, 136),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-                Visibility(
-                  visible: _tapped,
-                  child: SizedBox(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          height: 36,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: TextField(
-                                  controller: _txtController,
-                                  autofocus: true,
-                                  textAlignVertical: TextAlignVertical.center,
-                                  onSubmitted: _handleSubmitted,
-                                  decoration: const InputDecoration(
-                                    isDense: true,
-                                    border: InputBorder.none,
-                                    hintText: '오늘은 어떤 일을 할까요?',
-                                    hintStyle: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () =>
-                                    _handleSubmitted(_txtController.text),
-                                child: Container(
-                                  padding: const EdgeInsets.all(3),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: const Color.fromARGB(
-                                          255, 141, 255, 240)),
-                                  child:
-                                      const Icon(Icons.arrow_upward_outlined),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            const Icon(
+              Icons.menu,
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TodoWidget extends StatefulWidget {
-  const TodoWidget({super.key});
-
-  @override
-  State<TodoWidget> createState() => _TodoWidgetState();
-}
-
-class _TodoWidgetState extends State<TodoWidget> {
-  Widget leftSlideBackground() {
-    return Container(
-      alignment: Alignment.centerLeft,
-      decoration: const BoxDecoration(
-        color: Colors.lightGreen,
-      ),
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        child: const Icon(
-          Icons.check,
-          color: Colors.white,
-          size: 20,
-        ),
-      ),
-    );
-  }
-
-  Widget rightSlideBackground() {
-    return Container(
-      alignment: Alignment.centerRight,
-      decoration: const BoxDecoration(
-        color: Colors.red,
-      ),
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        child: const Icon(
-          Icons.delete,
-          color: Colors.white,
-          size: 20,
+          ],
         ),
       ),
     );
@@ -273,10 +62,11 @@ class _TodoWidgetState extends State<TodoWidget> {
 
   Widget todoListContainer(item) {
     return Container(
+      margin: const EdgeInsets.fromLTRB(12, 3, 12, 3),
       height: 42,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-      ),
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(13)),
       child: Row(
         children: <Widget>[
           // Row - Icon area
@@ -304,142 +94,41 @@ class _TodoWidgetState extends State<TodoWidget> {
 
   @override
   Widget build(BuildContext context) {
-    print('rebuild?>');
-    return ListView.builder(
-      itemCount: _txtList.length,
-      itemBuilder: (context, index) {
-        final String item = _txtList[index];
-        return Container(
-          margin: const EdgeInsets.fromLTRB(12, 3, 12, 3),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(13),
-            child: Dismissible(
-              key: Key(item),
-              background: leftSlideBackground(),
-              // Delete item
-              secondaryBackground: rightSlideBackground(),
-              // Complete item
-              onDismissed: (direction) {
-                setState(() {
-                  _txtList.removeAt(index);
-                });
-              },
-              child: todoListContainer(item),
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Column(
+              children: <Widget>[
+                titleInkWellBar(
+                    () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MyTodoPage()),
+                        ),
+                    '오늘 할 일'),
+              ],
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class TextFieldBar extends StatefulWidget {
-  const TextFieldBar({super.key});
-
-  @override
-  State<TextFieldBar> createState() => _TextFieldBarState();
-}
-
-class _TextFieldBarState extends State<TextFieldBar> {
-  final _txtController = TextEditingController();
-  bool _tapped = false;
-
-  void _handleSubmitted(String text) {
-    if (text != '') {
-      _txtList.add(text);
-      _txtController.clear();
-      _tapped = !_tapped;
-    }
-    _txtController.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double deviceWidth = MediaQuery.of(context).size.width;
-    final double deviceHeight = MediaQuery.of(context).size.height;
-
-    return GestureDetector(
-      onTap: () => setState(() {
-        _tapped = !_tapped;
-      }),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 100),
-        height: _tapped ? 100 : 40,
-        width: deviceWidth,
-        margin: const EdgeInsets.all(12),
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(13.0))),
-        child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: Column(
-            children: <Widget>[
-              Visibility(
-                visible: !_tapped,
-                child: Container(
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    '작업을 추가하려면 눌러주세요',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 136, 136, 136),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: _tapped,
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        height: 36,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Expanded(
-                              child: TextField(
-                                controller: _txtController,
-                                autofocus: true,
-                                textAlignVertical: TextAlignVertical.center,
-                                onSubmitted: _handleSubmitted,
-                                decoration: const InputDecoration(
-                                  isDense: true,
-                                  border: InputBorder.none,
-                                  hintText: '오늘은 어떤 일을 할까요?',
-                                  hintStyle: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () =>
-                                  _handleSubmitted(_txtController.text),
-                              child: Container(
-                                padding: const EdgeInsets.all(3),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: const Color.fromARGB(
-                                        255, 141, 255, 240)),
-                                child: const Icon(Icons.arrow_upward_outlined),
-                              ),
-                            )
-                          ],
+          Flexible(
+            flex: 1,
+            child: Column(
+              children: <Widget>[
+                titleInkWellBar(
+                    () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MyTodoPage()),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+                    '프로젝트'),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,6 +1,195 @@
 import 'package:flutter/material.dart';
 
-List<String> _txtList = [];
+class TodoForm {
+  final Key primaryKey;
+  final String taskName;
+  String? tag = 'nothing';
+  bool? isComplete = false;
+
+  TodoForm({required this.primaryKey, required this.taskName, this.tag});
+}
+
+List<TodoForm> incompleteList = [];
+List<TodoForm> completeList = [];
+
+class MyTodoPage extends StatefulWidget {
+  const MyTodoPage({super.key});
+
+  @override
+  State<MyTodoPage> createState() => _MyTodoPageState();
+}
+
+class _MyTodoPageState extends State<MyTodoPage> {
+  final _txtController = TextEditingController();
+  bool _isWhiteBarTapped = false;
+  bool _isGreenBarTapped = false;
+
+  void _handleSubmitted(String text) {
+    if (text != '') {
+      setState(() {
+        TodoForm newForm = TodoForm(
+          primaryKey: Key(text),
+          taskName: text,
+        );
+        incompleteList.add(newForm);
+        _txtController.clear();
+        _isWhiteBarTapped = !_isWhiteBarTapped;
+      });
+    }
+  }
+
+  Widget textFieldBar() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      height: 36,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              controller: _txtController,
+              autofocus: true,
+              textAlignVertical: TextAlignVertical.center,
+              onSubmitted: _handleSubmitted,
+              decoration: const InputDecoration(
+                isDense: true,
+                border: InputBorder.none,
+                hintText: '오늘은 어떤 일을 할까요?',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () => _handleSubmitted(_txtController.text),
+            child: Container(
+              padding: const EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(255, 141, 255, 240)),
+              child: const Icon(Icons.arrow_upward_outlined),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double deviceHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+      resizeToAvoidBottomInset: true,
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+            child: const Text(
+              '오늘 할 일',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Expanded(
+            child: TodoWidget(),
+          ),
+          // showingCompleteListBar
+          GestureDetector(
+            onTap: () => setState(() {
+              _isGreenBarTapped = !_isGreenBarTapped;
+            }),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOutCubic,
+              height: _isGreenBarTapped ? 250 : 40,
+              width: deviceWidth,
+              margin: const EdgeInsets.fromLTRB(12, 12, 12, 3),
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 101, 238, 96),
+                  borderRadius: BorderRadius.all(Radius.circular(13.0))),
+              child: SingleChildScrollView(
+                physics: const NeverScrollableScrollPhysics(),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 40,
+                  child: const Text(
+                    '완료된 일정',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 82, 82, 82),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // taskAddBar
+          GestureDetector(
+            onTap: () => setState(() {
+              _isWhiteBarTapped = !_isWhiteBarTapped;
+            }),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 100),
+              height: _isWhiteBarTapped ? 100 : 40,
+              width: deviceWidth,
+              margin: const EdgeInsets.fromLTRB(12, 3, 12, 12),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(13.0))),
+              child: SingleChildScrollView(
+                physics: const NeverScrollableScrollPhysics(),
+                child: Column(
+                  children: <Widget>[
+                    Visibility(
+                      visible: !_isWhiteBarTapped,
+                      child: Container(
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          '작업을 추가하려면 눌러주세요',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 136, 136, 136),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: _isWhiteBarTapped,
+                      child: SizedBox(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            textFieldBar(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class TodoWidget extends StatefulWidget {
   const TodoWidget({super.key});
@@ -78,139 +267,37 @@ class _TodoWidgetState extends State<TodoWidget> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: _txtList.length,
+      itemCount: incompleteList.length,
       itemBuilder: (context, index) {
-        final String item = _txtList[index];
+        final String item = incompleteList[index].taskName;
+        final Key primaryKey = incompleteList[index].primaryKey;
         return Container(
           margin: const EdgeInsets.fromLTRB(12, 3, 12, 3),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(13),
             child: Dismissible(
-              key: Key(item),
+              key: primaryKey,
               background: leftSlideBackground(),
               // Delete item
               secondaryBackground: rightSlideBackground(),
               // Complete item
               onDismissed: (direction) {
-                setState(() {
-                  _txtList.removeAt(index);
-                });
+                if (direction == DismissDirection.startToEnd) {
+                  setState(() {
+                    incompleteList[index].isComplete = true;
+                    completeList.add(incompleteList[index]);
+                  });
+                } else {
+                  setState(() {
+                    incompleteList.removeAt(index);
+                  });
+                }
               },
               child: todoListContainer(item),
             ),
           ),
         );
       },
-    );
-  }
-}
-
-class TextFieldBar extends StatefulWidget {
-  const TextFieldBar({super.key});
-
-  @override
-  State<TextFieldBar> createState() => _TextFieldBarState();
-}
-
-class _TextFieldBarState extends State<TextFieldBar> {
-  final _txtController = TextEditingController();
-  bool _tapped = false;
-
-  void _handleSubmitted(String text) {
-    if (text != '') {
-      _txtList.add(text);
-      _txtController.clear();
-      _tapped = !_tapped;
-    }
-    _txtController.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double deviceWidth = MediaQuery.of(context).size.width;
-    final double deviceHeight = MediaQuery.of(context).size.height;
-
-    return GestureDetector(
-      onTap: () => setState(() {
-        _tapped = !_tapped;
-      }),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 100),
-        height: _tapped ? 100 : 40,
-        width: deviceWidth,
-        margin: const EdgeInsets.all(12),
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(13.0))),
-        child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          child: Column(
-            children: <Widget>[
-              Visibility(
-                visible: !_tapped,
-                child: Container(
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    '작업을 추가하려면 눌러주세요',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 136, 136, 136),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: _tapped,
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        height: 36,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Expanded(
-                              child: TextField(
-                                controller: _txtController,
-                                autofocus: true,
-                                textAlignVertical: TextAlignVertical.center,
-                                onSubmitted: _handleSubmitted,
-                                decoration: const InputDecoration(
-                                  isDense: true,
-                                  border: InputBorder.none,
-                                  hintText: '오늘은 어떤 일을 할까요?',
-                                  hintStyle: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () => _handleSubmitted(_txtController.text),
-                              child: Container(
-                                padding: const EdgeInsets.all(3),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: const Color.fromARGB(255, 141, 255, 240)),
-                                child: const Icon(Icons.arrow_upward_outlined),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
